b0bfaa59126cb9b785e74d11acd48d4ed38eaef6
Gautham Elango <43374544+gauthamdaze@users.noreply.github.com>
2022-03-09 17:55:39 +0530

    Initial commit

---
+++ src/timer.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const Timer = () => {
+  const [second, setSecond] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [hour, setHour] = useState("00");
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60);
+        const hourCounter = Math.floor(counter / 3600);
+
+        let computedSecond =
+          String(secondCounter).length === 1
+            ? `0${secondCounter}`
+            : secondCounter;
+        let computedMinute =
+          String(minuteCounter).length === 1
+            ? `0${minuteCounter}`
+            : minuteCounter;
+        let computedHour =
+          String(hourCounter).length === 1 ? `0${hourCounter}` : hourCounter;
+
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+        setHour(computedHour);
+
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter]);
+
+  function stopTimer() {
+    setIsActive(false);
+    setCounter(0);
+    setSecond("00");
+    setMinute("00");
+    setHour("00");
+  }
+
+  return (
+    <div class="container">
+      <div class="time">
+        <span class="minute">{hour}</span>
+        <span>:</span>
+        <span class="minute">{minute}</span>
+        <span>:</span>
+        <span class="second">{second}</span>
+      </div>
+      <div class="buttons">
+        <button onClick={() => setIsActive(!isActive)} class="start">
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button onClick={stopTimer} class="reset">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
